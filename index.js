4fdfdd81b276d516e52e641f4876ee33825503ca
andjosh <email@andjosh.com>
2016-04-03 18:06:48 -0500

    Add logging

--- index.js
+++ index.js
@@ -34,19 +34,23 @@ app.get('/create/:name', function(req, res) {
     exec('convert ' + namedFile + ' ' + namedPng);
     imgPattern.src = fs.readFileSync(namedPng);
     img.src = fs.readFileSync(__dirname + '/public/narro.png');
+    console.info('draw start');
     canvasPattern = ctx.createPattern(imgPattern, 'repeat');
     ctx.fillStyle = canvasPattern;
     ctx.fillRect(0, 0, 1400, 1400);
     ctx.drawImage(img, 0, 0, 1400, 1400);
     stream = canvas.pngStream();
+    console.info('stream start');
     stream.on('data', function(chunk){
         out.write(chunk);
     });
     stream.on('end', function(){
+        console.info('stream end');
         s3Thing(namedFinal, req.params.name + '.png', 'narro-images',
         function(err, data, location) {
+            console.info('upload end');
             res.setStatus(201);
-            res.json({location: location});
+            res.jsonp({location: location});
         });
     });
 });
